ac5e01d87f5e9ff7b76ac018129e423f6b5136c6
giulia <ggd94@hotmail.it>
2016-05-03 22:11:04 +0200

    first commit

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,16 +1,20 @@
 var marker_array=[];
 var contentString='';
-      
+var array_luoghi= array_luoghi_temp;
+var array_foto= array_foto_temp;
+var map;
 function initMap(){
-    var map=new google.maps.Map(document.getElementById('googleMap'),{
+    map=new google.maps.Map(document.getElementById('googleMap'),{
         center: {lat: 46.10370807092794, lng: 11.865234375},
         zoom:3
     });
     var len=array_luoghi.length;
     for(var count=0;count<len;count++){
-        var location={lat :array_luoghi[count].place.location.latitude, lng: array_luoghi[count].place.location.longitude};
-        var nome= array_luoghi[count].place.location.city;
-        addMarker(location,map,nome);
+        if(array_luoghi[count].place.hasOwnProperty('location') &&  array_luoghi[count].place.location.hasOwnProperty('latitude')){
+            var location={lat :array_luoghi[count].place.location.latitude, lng: array_luoghi[count].place.location.longitude};
+            var nome= array_luoghi[count].place.location.city;
+            addMarker(location,map,nome);
+        }
     }
     
     for(var count=0;count<marker_array.length;count++){
@@ -45,4 +49,72 @@ function add_images(marker){
         }
     }
 }
+
+function place(){
+    var luogo=document.getElementById('place').value;
+    var geocoder= new google.maps.Geocoder();
+    geocoder.geocode({'address': luogo},function(results,status){
+        if(status == google.maps.GeocoderStatus.OK){
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(10);
+            var proprieta = {
+                path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+                fillColor: 'yellow',
+                fillOpacity: 0.8,
+                scale: 0.3,
+                strokeColor: 'gold',
+                strokeWeight: 6
+
+            };
+            var marker=new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                icon: proprieta
+            });
+            var content='';
+            var cont=0;
+            marker.addListener('click',function(){
+                console.log("ARRAY LEN "+array_friends.length);
+                for(var p=0;p<array_friends.length;p++){
+                    $.post('https://application-giulia.rhcloud.com/users/luogoCercato',{friend:array_friends[p].id,place: luogo},function(data){
+                        if(data.name != ''){
+                            content+='<p>'+data.name+'</p> <br>';
+                            console.log(content);
+                        }
+                        cont=cont+1;
+                    });
+                }
+                console.log(cont);
+                console.log(content);
+                var infowindow = new google.maps.InfoWindow({
+                    content: content
+                });
+                infowindow.open(map,this);
+            });
+        }
+        else{
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
+function friends(){
+    var html='';
+    for(var j=0;j<array_friends.length;j++){
+        html+= '<a onclick="friends_map('+array_friends[j].id+')" >'+array_friends[j].name+'</a> <br>';
+    }
+    document.getElementById('zonaDinamica').innerHTML=html;
+}
+
+function friends_map(id_user){
+            var url='https://application-giulia.rhcloud.com/users/dataFriends';
+            var form=$('<form action="'+ url +'" method="post">'+
+                '<input type="hidden" name="id" value="'+id_user+'">'+
+                '</form>');
+            $('body').append(form);
+            form.submit();
+}
+    
+                
+    
 
--- routes/users.js
+++ routes/users.js
@@ -9,112 +9,117 @@ var crypto =require('crypto');
 var APP_ID_FACEBOOK='';
 var APP_SECRET_FB='';
 var URL_OAUTH='https://graph.facebook.com/v2.6/oauth/access_token';
-var URL='https://www.facebook.com/dialog/oauth?client_id='+APP_ID_FACEBOOK+'&redirect_uri=https://application-giulia.rhcloud.com/users/FBLogin/confirm&scope=email,user_location,user_hometown,user_tagged_places,user_photos';
-var ACCESS_TOKEN,APPSECRET_PROOF;
-
-var USERNAME,ID,EMAIL,PHOTOS=[],IMAGE;
-var LUOGHI;
+var URL='https://www.facebook.com/dialog/oauth?client_id='+APP_ID_FACEBOOK+'&redirect_uri=https://application-giulia.rhcloud.com/users/FBLogin/confirm&scope=email,user_location,user_hometown,user_tagged_places,user_photos,user_friends';
 
 /*funzione invocata al click del bottone per login facebook*/
 router.get('/FBLogin',function(req,res,next){
-    res.redirect(URL);
+    res.redirect('https://application-giulia.rhcloud.com/users/FBLogin/confirm');
 });
 
-/*login tramite facebook chiedendo conferma al client*/
+/*-----------------------------------login tramite facebook chiedendo conferma al client-----------------------------*/
 router.get('/FBLogin/confirm',function(req,res,next){
-    var code=req.query.code;
-    request.get({
-        url: URL_OAUTH,
-        qs: {
-            client_id: APP_ID_FACEBOOK,
-            redirect_uri: 'https://application-giulia.rhcloud.com/users/FBLogin/confirm',
-            client_secret:APP_SECRET_FB,
-            code:code
-        }
-    },function(error,response,body){
-        if(!error && response.statusCode==200){
-            var element=JSON.parse(body);
-            ACCESS_TOKEN = element.access_token;
-            APPSECRET_PROOF=crypto.createHmac('SHA256',APP_SECRET_FB).update(ACCESS_TOKEN).digest('hex');
-            
-            request.get({
-                url: 'https://graph.facebook.com/v2.6/me/photos',
-                qs:{
-                    fields:"images,place",
-                    access_token: ACCESS_TOKEN,
-                    appsecret_proof: APPSECRET_PROOF
-                }
-            },function(error,response,body){
-                if(!error && response.statusCode==200){
-                    var array=JSON.parse(body).data;
-                    for(var i=0;i<array.length;i++){
-                        if(array[i].hasOwnProperty('place')){
+    var USERNAME,ID,EMAIL,PHOTOS=[],IMAGE,FRIENDS,LUOGHI;
+    var ACCESS_TOKEN,APPSECRET_PROOF;
+    console.log(req.query);
+    if(!req.query.hasOwnProperty('code')){
+        res.redirect(URL);
+    }
+    else{
+        var code=req.query.code;
+        request.get({
+            url: URL_OAUTH,
+            qs: {
+                client_id: APP_ID_FACEBOOK,
+                redirect_uri: 'https://application-giulia.rhcloud.com/users/FBLogin/confirm',
+                client_secret:APP_SECRET_FB,
+                code:code
+            }
+        },function(error,response,body){
+            if(!error && response.statusCode==200){
+                var element=JSON.parse(body);
+                ACCESS_TOKEN = element.access_token;
+                APPSECRET_PROOF=crypto.createHmac('SHA256',APP_SECRET_FB).update(ACCESS_TOKEN).digest('hex');
+                request.get({
+                    url: 'https://graph.facebook.com/v2.6/me/photos',
+                    qs:{
+                        fields:"images,place",
+                        access_token: ACCESS_TOKEN,
+                        appsecret_proof: APPSECRET_PROOF
+                    }
+                },function(error,response,body){
+                    if(!error && response.statusCode==200){
+                        var array=JSON.parse(body).data;
+                        for(var i=0;i<array.length;i++){
+                            if(array[i].hasOwnProperty('place')){
                                 PHOTOS.push(array[i]);
+                            }
                         }
                     }
-                }
-                else{
-                    console.log(error);
-                }
-            });
-            
-            request.get({
-                url: 'https://graph.facebook.com/v2.6/me',
-                qs:{
-                    fields:"name,email,picture,tagged_places,hometown,location",
-                    access_token: ACCESS_TOKEN,
-                    appsecret_proof: APPSECRET_PROOF
-                }
-            },function(error,response,body){
-                if(!error && response.statusCode==200){
-                    USERNAME=JSON.parse(body).name;
-                    ID=JSON.parse(body).id;
-                    EMAIL=JSON.parse(body).email;
-                    IMAGE=JSON.parse(body).picture.data.url;
-                    LUOGHI=JSON.parse(body).tagged_places.data;
-/************************** SALVO DATI IN FIREBASE ********************************/
-
-                    var FIREBASE_URL='https://application-giulia.firebaseio.com/Users.json'
-                    var requestData= {
-                        "Person" : {
-                            id: ID,
-                            user : USERNAME,
-                            email: EMAIL,
-                            image: IMAGE,
-                            luoghi:LUOGHI,
-                            photos: PHOTOS
-    
-                        }
-                    };
-                    request.put({
-                        url: FIREBASE_URL,
-                        json: true,
-                        headers: {
-                            "content-type": "application/json"
-                        },
-                        body: JSON.stringify(requestData)
+                    else{
+                        console.log(error);
+                    }
+                });
+                        
+                request.get({
+                    url: 'https://graph.facebook.com/v2.6/me',
+                    qs:{
+                        fields:"name,email,picture,tagged_places,friends",
+                        access_token: ACCESS_TOKEN,
+                        appsecret_proof: APPSECRET_PROOF
+                    }
                     },function(error,response,body){
                         if(!error && response.statusCode==200){
-                            res.render("home",{name: USERNAME,email: EMAIL,photo: IMAGE,luoghi: JSON.stringify(LUOGHI),photos: JSON.stringify(PHOTOS)});
-                        }
-                        else{
-                            console.log(error);
-                        }
-                    });
-                    
-/*************************** FINE SALVATAGGIO ***************************************/
-                    
-                }
-                else{
-                    console.log(error);
-                }
-            });
-        }
-        else{
-            console.log(error);
-        }
+                            USERNAME=JSON.parse(body).name;
+                            ID=JSON.parse(body).id;
+                            EMAIL=JSON.parse(body).email;
+                            IMAGE=JSON.parse(body).picture.data.url;
+                            LUOGHI=JSON.parse(body).tagged_places.data;
+                            FRIENDS=JSON.parse(body).friends.data;
+            /************************** SALVO DATI IN FIREBASE ********************************/
 
-    });
+                            var FIREBASE_URL='https://application-giulia.firebaseio.com/Users/'+ID+'.json';
+                            var requestData= {
+                                "Person" : {
+                                    user : USERNAME,
+                                    email: EMAIL,
+                                    image: IMAGE,
+                                    luoghi:LUOGHI,
+                                    photos: PHOTOS,
+                                    friends: FRIENDS
+                                }
+                            };
+                            request.put({
+                                url: FIREBASE_URL,
+                                json: true,
+                                headers: {
+                                    "content-type": "application/json"
+                                },body: JSON.stringify(requestData)
+                            },function(error,response,body){
+                                if(!error && response.statusCode==200){
+                                    res.render("home",{
+                                        name: USERNAME,
+                                        email: EMAIL,
+                                        photo: IMAGE,
+                                        luoghi: JSON.stringify(LUOGHI),
+                                        photos: JSON.stringify(PHOTOS),
+                                        friends: JSON.stringify(FRIENDS)
+                                    });
+                                }
+                                else{
+                                    console.log(error);
+                                }
+                            });
+                                
+            /*************************** FINE SALVATAGGIO ***************************************/
+                                
+                        }
+                    else{
+                        console.log(error);
+                    }
+                });
+            }
+        });
+    }
 });
 
 /*---------------------------------------------------------------------------------------------FINE LOGIN FACEBOOK------------------------------------------------------------------*/
@@ -124,10 +129,63 @@ router.get('/FBLogin/confirm',function(req,res,next){
 
     
 
-
+router.post('/dataFriends',function(req,res,next){
+    var id=req.body.id;
+    var url='https://application-giulia.firebaseio.com/Users/'+id+'.json';
+    request.get({
+        url: url,
+        json: true,
+        headers: {
+            "content-type": "application/json"
+        }
+        },function(error,response,body){
+            if(!error && response.statusCode==200){
+                var persona=JSON.parse(response.body);
+                res.render('home_friends',{
+                    name:''+ persona.Person.user,
+                    email:''+persona.Person.email,
+                    image:''+persona.Person.image,
+                    luoghi:JSON.stringify(persona.Person.luoghi),
+                    photos:JSON.stringify(persona.Person.photos)
+                });
+            }
+            else{
+                res.json(response.statusCode,{
+                    error: error
+                });
+            }
+        });
+});
+        
+        
+router.post('/luogoCercato',function(req,res,next){
+    var luogo=req.body.place;
+    var id=req.body.friend;
+    var url='https://application-giulia.firebaseio.com/Users/'+id+'.json';
+    request.get({
+        url: url,
+        json: true,
+        headers: {
+            "content-type": "application/json"
+        }
+    },function(error,response,body){
+        if(!error && response.statusCode==200){
+            var nome=JSON.parse(response.body).Person.user;
+            var place=JSON.parse(response.body).Person.luoghi;
+            for(var l=0;l<place.length;l++){
+                if(place[l].place.location.city.toLowerCase() == luogo.toLowerCase())
+                    res.json({name:''+nome});
+            }
+        }
+        else{
+            console.log(error);
+            res.json({name:''});
+        }
+    });
+});
 
 /*gestione della url di HOMEPAGE una volta loggati*/
-router.get('/',function(req,res,next){
+router.post('/',function(req,res,next){
     var options = {
         root: __dirname + "/../public/html/",
         headers: {
